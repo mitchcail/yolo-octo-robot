b9bbfe405b1230c43fa2f8aff974c8f7e92d783e
mitch cail <mitch.cail@gmail.com>
2014-09-20 22:36:52 -0400

    added front end button for like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+	before_action :authenticate_user!
+
+
+	def create
+		@post = Post.find(params[:post_id])
+		@post.likes.create!(user: current_user)
+
+		#check to see if the user already likes this post
+		@current_like = current_user.likes.find_by(post_id: @post.id)
+
+		redirect_to posts_url
+	end
 end
