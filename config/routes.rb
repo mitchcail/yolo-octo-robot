3f450c8c6c7c26ad62d7d3df8ce05f7921242631
mitch cail <mitch.cail@gmail.com>
2014-09-20 21:55:50 -0400

    added a like button

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   resources :locations
 
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
