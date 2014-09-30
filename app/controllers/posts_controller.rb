26561733bf57660f28fa4b7ab9cd155c2e8b4eae
mitch cail <mitch.cail@gmail.com>
2014-09-30 13:40:58 -0400

    yet more heroku schtuff to get this app to work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,6 +49,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     puts @post.user = current_user
+    current_user.geocode
     puts @post.longitude = current_user.longitude
     puts @post.latitude = current_user.latitude
     @post.radius = 40.0
