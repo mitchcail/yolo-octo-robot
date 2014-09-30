ee8eb8a5d6fb0a29d4b7547bfc1e8f6463997333
mitch cail <mitch.cail@gmail.com>
2014-09-30 13:43:18 -0400

    geocode when user hits front page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
     if user_signed_in?
       @feed = []
       @user = current_user
+      current_user.geocode
       Post.all.each do |post|
         add_feed(@user, post)
       end
